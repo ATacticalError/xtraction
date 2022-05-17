01bd7214370068c1acd109f59029ce2c6d5abdb7
GlitchyRobot <42768914+GlitchyRobot@users.noreply.github.com>
2022-05-17 18:50:47 +1000

    Menu Update
    Added a Master Volume
    The options section has a slider to modify this master volume. Requires sound to test.
    Added Credits section.

--- Assets/Scripts/MenuSwitch.cs
+++ Assets/Scripts/MenuSwitch.cs
@@ -7,23 +7,34 @@ public class MenuSwitch : MonoBehaviour
     public GameObject MainObject;
     public GameObject OptionsObject;
     public GameObject AboutObject;
+    public GameObject CreditsObject;
 
     public void Options()
     {
         MainObject.SetActive(false);
         AboutObject.SetActive(false);
         OptionsObject.SetActive(true);
+        CreditsObject.SetActive(false);
     }
     public void Main()
     {
         MainObject.SetActive(true);
         AboutObject.SetActive(false);
         OptionsObject.SetActive(false);
+        CreditsObject.SetActive(false);
     }
     public void About()
     {
         MainObject.SetActive(false);
         AboutObject.SetActive(true);
         OptionsObject.SetActive(false);
+        CreditsObject.SetActive(false);
+    }
+    public void Credits()
+    {
+        MainObject.SetActive(false);
+        AboutObject.SetActive(false);
+        OptionsObject.SetActive(false);
+        CreditsObject.SetActive(true);
     }
 }
---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        mixer.SetFloat("Volume", (sliderValue * 100) - 80);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
